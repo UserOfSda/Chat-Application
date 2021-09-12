56c79bc112a5abf705add8c3518b8da978cfadc9
Tarmo Pärnaku <tarmoparnaku@gmail.com>
2021-09-12 14:41:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var PORT = process.env.PORT || 3000; // take port from heroku or for loacalhost
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
